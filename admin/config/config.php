decc0c0437da3195eb47ab827cb72a1ee7e7b3d9
DarkDub <dubaimon1010@gmail.com>
2025-06-15 17:31:22 -0500

    ignore

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,8 +1,5 @@
 <?php
 
-define("CLIENT_ID", "Ad4mFrgbNaa4ulZNi7Plx0-e_jGeHV80zQYcwYjHaFwXvQ_Y2xLBYxbhb7xZiAXNa6IbYrlmM2MUx0vM");
-define("CURRENCY", "USD");/* esto es una constante el nombre es en mayuscula y la clave para esa constante*/
-define("KEY_TOKEN", "ABR.uli-021*");/* esto es una constante el nombre es en mayuscula y la clave para esa constante*/
 define("MONEDA", "$");  
 
 session_start();
